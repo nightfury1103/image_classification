1e88ae8eb529537e4670669853c2e68936620c5a
nightfury1103 <huyquochoan2@gmail.com>
2024-03-25 17:08:02 +0700

    monday with classification

--- CLIP.py
+++ CLIP.py
@@ -1,62 +1,46 @@
-import requests
+import torch
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from data_utils import INTELDataset
-import torch
 from tqdm import tqdm
 
+# Initialize the processor and model
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-# Check for GPU availability
+# Check for GPU availability and move the model to the selected device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 print(f"Using device: {device}")
 
-# Move the model to the selected device
-model.to(device)
-
 batch_size = 16
-
 train_loader = INTELDataset(batch_size=batch_size).get_train_dataloader()
-epochs = range(1)
-
-for image, label, caption in train_loader:
-    image = Image.open(image[0])
-    inputs = processor(image, label[0], return_tensors="pt")
-    inputs = {k: v.to(device) for k, v in inputs.items()}
-    # inputs['labels'] = processor(text=caption[0], return_tensors="pt")["input_ids"]
-
-    outputs = model(**inputs)
-    break
-print(outputs.loss)
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
 
 model.train()
-optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
-for epoch in epochs:
+
+for epoch in range(1):
     for i, (image_paths, labels, captions) in enumerate(tqdm(train_loader)):
         batch_images = []
-        # Process each image in the batch
         for image_path in image_paths:
-            with Image.open(image_path) as img:
-                img = img.convert("RGB")  # Ensure image is in RGB
-                batch_images.append(img)
-                
-        inputs = processor(batch_images, labels, return_tensors="pt", padding=True, truncation=True)
+            image = Image.open(image_path).convert("RGB")
+            batch_images.append(image)
+            
+        # Process images and captions and then move processed data to the correct device
+        inputs = processor(images=batch_images, text=captions, return_tensors="pt", padding=True, truncation=True)
         inputs = {k: v.to(device) for k, v in inputs.items()}
 
+        # Forward pass
         outputs = model(**inputs)
         loss = outputs.loss
-        model.zero_grad()
+        
+        # Backward and optimize
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         if i % 100 == 0:
             print(f"Epoch {epoch}, Iteration {i}, Loss: {loss.item()}")
-print(outputs.loss)
-
-# Define your save path
-model_save_path = 'classification.pt'
-
-# Save the model state dict
-torch.save(model.state_dict(), model_save_path)
-
+            
+# Save the model's state_dict
+torch.save(model.state_dict(), 'classification.pt')
