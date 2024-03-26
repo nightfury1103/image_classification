bd5237147865135e1b4904c3fc4e86d4f6871611
nightfury1103 <huyquochoan2@gmail.com>
2024-03-26 17:10:15 +0700

    tuesday with add label tensor

--- CLIP.py
+++ CLIP.py
@@ -4,6 +4,19 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 from data_utils import INTELDataset
 from tqdm import tqdm
 import numpy as np
+import random
+from metrics import tokenize_and_pad_texts
+
+seed = 42
+torch.manual_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
+
+# Ensure deterministic behavior in CuDNN (if using CUDA)
+if torch.backends.cudnn.enabled:
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 
 # Initialize the processor and model
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
@@ -14,7 +27,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 print(f"Using device: {device}")
 
-batch_size = 4
+batch_size = 1
 train_loader = INTELDataset(batch_size=batch_size).get_train_dataloader()
 valid_loader = INTELDataset(batch_size=batch_size).get_valid_dataloader()
 optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
@@ -32,7 +45,15 @@ for epoch in range(5):
             batch_images.append(image)
             
         # Classification task
-        inputs = processor(images=batch_images, text=labels, return_tensors="pt", padding=True, truncation=True)
+        inputs = processor(images=batch_images, text=['predict: '] * batch_size, return_tensors="pt", padding=True, truncation=True)
+        encode_labels = processor(images=batch_images, text=list(labels), return_tensors="pt", padding=True, truncation=True)
+
+        token_1 = inputs['input_ids']
+        token_2 = encode_labels['input_ids']
+        token_1, token_2 = tokenize_and_pad_texts(token_1, token_2)
+        
+        inputs["input_ids"] = token_1
+        inputs["labels"] = token_2
         inputs = {k: v.to(device) for k, v in inputs.items()}
 
         # Forward pass
@@ -40,7 +61,23 @@ for epoch in range(5):
         classification_loss = classification_output.loss
         
         # Captioning task
-        inputs = processor(images=batch_images, text=captions, return_tensors="pt", padding=True, truncation=True)
+        inputs = processor(images=batch_images, text=['describe: '] * batch_size, return_tensors="pt", padding=True, truncation=True, max_length=256)
+        encode_captions = processor(images=batch_images, text=list(captions), return_tensors="pt", padding=True, truncation=True)
+
+        token_1 = inputs['input_ids']
+        token_2 = encode_captions['input_ids']
+        token_1, token_2 = tokenize_and_pad_texts(token_1, token_2)
+
+        inputs["input_ids"] = token_1
+        inputs["labels"] = token_2
+        
+        sequence_length_diff = inputs["input_ids"].shape[1] - inputs["attention_mask"].shape[1]
+        if sequence_length_diff > 0:
+            # Apply padding to the sequence length dimension (the last dimension)
+            attention_mask_padded = torch.nn.functional.pad(inputs['attention_mask'], (0, sequence_length_diff), value=0)
+        
+        inputs['attention_mask'] = attention_mask_padded
+
         inputs = {k: v.to(device) for k, v in inputs.items()}
 
         # Forward pass
@@ -71,7 +108,15 @@ for epoch in range(5):
                 batch_images.append(image)
 
             # Classification task
-            inputs = processor(images=batch_images, text=labels, return_tensors="pt", padding=True, truncation=True)
+            inputs = processor(images=batch_images, text=['predict: '] * batch_size, return_tensors="pt", padding=True, truncation=True)
+            encode_labels = processor(images=batch_images, text=list(labels), return_tensors="pt", padding=True, truncation=True)
+
+            token_1 = inputs['input_ids']
+            token_2 = encode_labels['input_ids']
+            token_1, token_2 = tokenize_and_pad_texts(token_1, token_2)
+            
+            inputs["input_ids"] = token_1
+            inputs["labels"] = token_2
             inputs = {k: v.to(device) for k, v in inputs.items()}
 
             # Forward pass
@@ -79,7 +124,23 @@ for epoch in range(5):
             classification_loss = classification_output.loss
 
             # Captioning task
-            inputs = processor(images=batch_images, text=captions, return_tensors="pt", padding=True, truncation=True)
+            inputs = processor(images=batch_images, text=['describe: '] * batch_size, return_tensors="pt", padding=True, truncation=True, max_length=256)
+            encode_captions = processor(images=batch_images, text=list(captions), return_tensors="pt", padding=True, truncation=True)
+
+            token_1 = inputs['input_ids']
+            token_2 = encode_captions['input_ids']
+            token_1, token_2 = tokenize_and_pad_texts(token_1, token_2)
+
+            inputs["input_ids"] = token_1
+            inputs["labels"] = token_2
+            
+            sequence_length_diff = inputs["input_ids"].shape[1] - inputs["attention_mask"].shape[1]
+            if sequence_length_diff > 0:
+                # Apply padding to the sequence length dimension (the last dimension)
+                attention_mask_padded = torch.nn.functional.pad(inputs['attention_mask'], (0, sequence_length_diff), value=0)
+            
+            inputs['attention_mask'] = attention_mask_padded
+
             inputs = {k: v.to(device) for k, v in inputs.items()}
 
             # Forward pass
@@ -104,4 +165,4 @@ for epoch in range(5):
 
 
 # Save the model's state_dict
-torch.save(model.state_dict(), 'classification.pt')
+torch.save(model.state_dict(), 'classify.pt')
--- metrics.py
+++ metrics.py
@@ -1,7 +1,19 @@
 import torch
 import torch.nn as nn
 from torch.nn import CrossEntropyLoss
+import torch.nn.functional as F
 
 criterion_class = nn.CrossEntropyLoss()
 criterion_desc = nn.CrossEntropyLoss()  # For simplicity, adjust for sequence generation
 
+def tokenize_and_pad_texts(tokens1: str, tokens2: str, padding_value: int = 0):
+    # Determine the maximum length
+    max_length = max(tokens1.shape[1], tokens2.shape[1])
+    
+    # Pad the tokenized texts to the same length
+    if tokens1.shape[1] < max_length:
+        tokens1 = torch.nn.functional.pad(tokens1, (0, max_length - tokens1.shape[1]), value=padding_value)
+    if tokens2.shape[1] < max_length:
+        tokens2 = torch.nn.functional.pad(tokens2, (0, max_length - tokens2.shape[1]), value=padding_value)
+    
+    return tokens1, tokens2
